43feb3e77facf323642287fa6fa781ca6aaa669a
DaDark12 <188825825+DaDark12@users.noreply.github.com>
2025-11-06 18:41:14 +0400

    Add prayerLabels array to prayers.js

---
+++ prayers.js
@@ -0,0 +1,7 @@
+export const prayerLabels = [
+  { name: "Fajr", key: "Fajr" },
+  { name: "Sunrise", key: "Sunrise" },
+  { name: "Dhuhrain", key: ["Dhuhr", "Asr"] },
+  { name: "Sunset", key: "Sunset" },
+  { name: "Maghribain", key: ["Maghrib", "Isha"] },
+];
